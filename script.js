d1f18f85e19c42a56b7229202befa599c2f5052d
aniketpallavi131225 <aniketyadav2615@gmail.com>
2025-12-19 10:16:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function sayHello() {
   alert("Thanks for visiting my portfolio 😊");
 }
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+
+  if (nav.style.display === "flex") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "flex";
+  }
+}
